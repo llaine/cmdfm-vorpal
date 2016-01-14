2f1e20716e2a74f24d13efa0414828801d5db7bc
Louis Lainé <louis.laine7@gmail.com>
2016-01-14 18:08:19 +0100

    Début du menu, réimport des autres libs

--- src/app.js
+++ src/app.js
@@ -1,5 +1,32 @@
 'use strict';
 
-import Greeting from './toto';
+import Vorpal from 'vorpal';
+import Menu from './menu';
 
-new Greeting('toto');
+let vorpal = new Vorpal();
+vorpal.find('exit').remove();
+
+let menu = new Menu();
+
+// This command display the specific musical styles available.
+vorpal
+    .command('styles', 'Display the musical style availables')
+    .action(function(args, callback) {
+      this.log('toto')
+      callback();
+    });
+
+// This command launch a playlist
+vorpal
+    .command('play <style>', 'Launch a playlist with a specific style')
+    .action(function(args, callback) {
+      this.log(args);
+      callback();
+    });
+
+// The exit command, that basically quit the app.
+vorpal.command('exit').action(() => console.log('Ciao !'));
+vorpal
+    .delimiter('cmdfm$')
+    .show();
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,14 @@
 var path = require('path');
+var fs = require('fs');
+
+var nodeModules = {};
+fs.readdirSync('node_modules')
+    .filter(function(x) {
+      return ['.bin'].indexOf(x) === -1;
+    })
+    .forEach(function(mod) {
+      nodeModules[mod] = 'commonjs ' + mod;
+    });
 
 module.exports = {
   entry: "./src/app.js",
@@ -6,12 +16,13 @@ module.exports = {
     path: "./dist",
     filename: "bundle.js"
   },
+  externals: nodeModules,
   devtool: 'inline-source-map',
   module: {
     loaders: [
       {
         test: path.join(__dirname, 'src'),
-        loader: 'babel-loader'
+        loader: 'babel'
       }
     ]
   }
